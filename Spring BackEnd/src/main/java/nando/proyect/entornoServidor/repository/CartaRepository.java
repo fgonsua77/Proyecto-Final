c094e6f696c1c1e257a553f3ca8ca9cc6dc1593a
Feяnando <62938201+fgonsua77@users.noreply.github.com>
2022-04-18 12:22:45 +0200

    Preparando SQLite

--- Spring BackEnd/src/main/java/nando/proyect/entornoServidor/repository/CartaRepository.java
+++ Spring BackEnd/src/main/java/nando/proyect/entornoServidor/repository/CartaRepository.java
@@ -2,7 +2,6 @@ package nando.proyect.entornoServidor.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import nando.proyect.entornoServidor.model.Carta;
 
 public interface CartaRepository extends JpaRepository<Carta, Integer> {
